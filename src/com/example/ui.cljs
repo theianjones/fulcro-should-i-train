c1c92e0db4aba63f00e50b5156aee8f4e286abbe
theianjones <jones58ian@gmail.com>
2021-12-21 11:31:54 -0500

    broken form

--- src/com/example/ui.cljs
+++ src/com/example/ui.cljs
@@ -1,15 +1,18 @@
 (ns com.example.ui
   (:require
-   [com.example.mutations :as mut]
+   [com.example.mutations :as mut :refer [clear-readiness-form readiness-ident]]
+   [com.fulcrologic.fulcro.algorithms.form-state :as fs]
    [com.fulcrologic.fulcro.algorithms.merge :as merge]
    [com.fulcrologic.fulcro.algorithms.tempid :as tempid]
    [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]
    [com.fulcrologic.fulcro.algorithms.normalized-state :as norm]
    [com.fulcrologic.fulcro.components :as comp :refer [defsc transact!]]
    [com.fulcrologic.fulcro.raw.components :as rc]
+   [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
    [com.fulcrologic.fulcro.data-fetch :as df]
-   [com.fulcrologic.fulcro.dom :as dom :refer [button div form h1 h2 h3 input label li ol p ul]]
-   [com.example.supabase :refer [github-signin github-signout]]))
+   [com.fulcrologic.fulcro.dom :as dom :refer [button div form h1 h2 h3 input label li ol p ul select option]]
+   [com.example.supabase :refer [github-signin github-signout]]
+   [com.example.data :refer [readiness-data]]))
 
 (defsc User [_ _]
   {:query [:user/avatar-url :user/email :user/id :user/name :user/username :user/authenticated?]})
@@ -30,24 +33,50 @@
 
 (def ui-header (comp/factory Header))
 
+(defn field [{:keys [label valid? error-message input-class options] :as props}]
+  (let [input-props (-> props
+                        (assoc :name label)
+                        (dissoc :label :valid? :error-message :input-class :options))]
+    (div :.ui.field
+         (dom/label {:htmlFor label} label)
+         (input-class input-props
+                      (when options (map #(option {:value (:value %) :key (:value %)} (:label %)) options)))
+         (dom/div :.ui.error.message {:classes [(when valid? "hidden")]}
+                  error-message))))
+
+(def t (-> readiness-data
+           :questions
+           first))
+
+(defsc ReadinessForm [this {:readiness/keys [trained]}]
+  {:query [:readiness/trained fs/form-config-join]
+   :ident (fn [] readiness-ident)
+   :initial-state (fn [_]
+                    (fs/add-form-config ReadinessForm {:readiness/trained ""}))
+   :form-fields #{:readiness/trained}
+   :componentDidMount (fn [this]
+                        (comp/transact! this [(clear-readiness-form nil)]))}
+  (div
+   (h3 "Readiness Form")
+   (field {:input-class   select
+           :options       (:options t)
+           :label         (:value t)
+           :value         (or trained (-> t
+                                          :options
+                                          first
+                                          :score))
+           :autoComplete  "off"
+           :onChange      #(m/set-integer! this :readiness/trained :event %)})))
+
+(def ui-readiness-form (comp/factory ReadinessForm))
+
 (defsc Root [this {:root/keys [user] :as props}]
   {:query [[df/marker-table :load-progress] :new-thing
            {:root/user (comp/get-query User)}
            [df/marker-table :load-user]
-           {:header (comp/get-query Header)}]
+           {:header (comp/get-query Header)}
+           {:readiness-form (comp/get-query ReadinessForm)}]
    :initial-state {:root/user {} :header {}}}
   (div
    (ui-header (comp/computed (:header props) {:on-signout #(comp/transact! this [(mut/delete-root-user nil)]) :user user}))
-   (p (str "Hello " (or (:user/name user) "from the ui/Root component") "!"))
-   (div {:style {:border "1px dashed", :margin "1em", :padding "1em"}}
-        (p "Invoke a load! that fails and display the error:")
-        (when-let [m (get props [df/marker-table :load-progress])]
-          (dom/p "Progress marker: " (str m)))
-        (button {:onClick #(df/load! this :i-fail (rc/nc '[*]) {:marker :load-progress})} "I fail!"))
-   (div {:style {:border "1px dashed", :margin "1em", :padding "1em"}}
-        (p "Simulate creating a new thing with server-assigned ID, leveraging Fulcro's tempid support:")
-        (button {:onClick #(let [tmpid (tempid/tempid)]
-                             (comp/transact! this [(mut/create-random-thing {:tmpid tmpid})]))}
-                "I create!")
-        (when-let [things (:new-thing props)]
-          (p (str "Created a thing with the ID: " (first (keys things))))))))
+   (ui-readiness-form (:readiness-form props))))
